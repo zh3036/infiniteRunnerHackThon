822700e9715769a7ef06ad5d036b784f25536334
zh3036 <zh3036@gmail.com>
2015-01-24 00:49:24 +0300

    initial
    first

---
+++ Assets/scripts/carmerarunner.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carmerarunner : MonoBehaviour {
+
+	public Transform player;
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.position.x + 5, 0, -10);
+	}
+}
---
+++ Assets/scripts/spanwn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spanwn : MonoBehaviour {
+
+
+	public GameObject [] obj;
+	public float spawnMin = 1f;
+	public float spawnMax = 2f;
+	public float timepsed=0f;
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+	void Spawn()
+	{
+		Instantiate(obj[Random.Range(0,obj.GetLength(0))],transform.position, Quaternion.identity);
+		print (Time.deltaTime);
+		timepsed = timepsed + Time.deltaTime;
+		print ("all time");
+		print (timepsed);
+		Invoke ("Spawn", Random.Range(1f,2f *(1f+timepsed)));
+	}
+}
